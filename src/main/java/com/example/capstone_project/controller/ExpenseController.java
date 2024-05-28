7692f90b0c78d1f695073875f9da409434aa692f
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-05-28 22:10:37 +0700

    [mock] add validation for ConfirmNewExpenseBody and ExpenseBody

--- src/main/java/com/example/capstone_project/controller/ExpenseController.java
+++ src/main/java/com/example/capstone_project/controller/ExpenseController.java
@@ -1,14 +1,31 @@
 package com.example.capstone_project.controller;
 
 import com.example.capstone_project.controller.body.confirm_expenses.ConfirmNewExpenseBody;
+import com.example.capstone_project.entity.AccessTokenClaim;
+import com.example.capstone_project.utils.helper.JwtHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/plan")
 @RequiredArgsConstructor
 public class ExpenseController {
-    @PutMapping("/upload")
-    public void selectTerm(@RequestBody(required = true) ConfirmNewExpenseBody body) {
+    private final JwtHelper jwtHelper;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ConfirmNewExpenseBody> confirmExpenses(
+            @RequestHeader("Authorization") String token,
+            @RequestBody ConfirmNewExpenseBody body) {
+        //Get access token
+        final String accessToken = token.substring(7);
+
+        //Get department ID
+        AccessTokenClaim accessTokenClaim = jwtHelper.parseToken(accessToken);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 }
--- src/main/java/com/example/capstone_project/controller/body/confirm_expenses/ConfirmNewExpenseBody.java
+++ src/main/java/com/example/capstone_project/controller/body/confirm_expenses/ConfirmNewExpenseBody.java
@@ -1,5 +1,7 @@
 package com.example.capstone_project.controller.body.confirm_expenses;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,8 +10,12 @@ import java.util.List;
 @Data
 @Builder
 public class ConfirmNewExpenseBody {
+    @NotEmpty(message = "Term Id, can't not be empty")
     private long termId;
+    @NotEmpty(message = "Term Id, can't not be empty")
     private String planName;
+    @NotEmpty(message = "Term Id, can't not be empty")
     private String fileName;
+    @NotNull(message = "Term Id, can't not be null")
     private List<ExpenseBody> expenses;
 }
