1bcf109293558c9747312a2894402b89bbd9b262
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-05-28 22:29:25 +0700

    [mock] add method get list cost type mock api

---
+++ src/main/java/com/example/capstone_project/controller/responses/confirm_expenses/CostTypeResponse.java
@@ -0,0 +1,11 @@
+package com.example.capstone_project.controller.responses.confirm_expenses;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CostTypeResponse {
+    private long costTypeId;
+    private String name;
+}
