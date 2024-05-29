0fe0bae5cbdced1bf2ba7f0d62cf222600ea31d0
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-05-29 18:38:20 +0700

    [mock] fix name mock api of list cost type

--- src/main/java/com/example/capstone_project/controller/CostTypeController.java
+++ src/main/java/com/example/capstone_project/controller/CostTypeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/costType")
+@RequestMapping("/api/cost-type")
 @RequiredArgsConstructor
 public class CostTypeController {
 
