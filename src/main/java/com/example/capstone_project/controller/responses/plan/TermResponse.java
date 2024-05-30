f5ffeed5832a5166e8eb73853bb6d08870273ede
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-05-30 19:15:14 +0700

    [mock] create plan detail mock api

--- src/main/java/com/example/capstone_project/controller/responses/plan/TermResponse.java
+++ src/main/java/com/example/capstone_project/controller/responses/plan/TermResponse.java
@@ -1,4 +1,4 @@
-package com.example.capstone_project.controller.responses.planManagement;
+package com.example.capstone_project.controller.responses.plan;
 
 import lombok.Builder;
 import lombok.Data;
