bad6a68c118185d2393a937890f7d7efaa97ea79
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-06-03 17:14:20 +0700

    [mock] create plan paging department mock api

---
+++ src/main/java/com/example/capstone_project/controller/DepartmentController.java
@@ -0,0 +1,60 @@
+package com.example.capstone_project.controller;
+
+import com.example.capstone_project.controller.responses.ListResponse;
+import com.example.capstone_project.controller.responses.Pagination;
+import com.example.capstone_project.controller.responses.department.paginate.DepartmentPaginate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/department")
+@RequiredArgsConstructor
+public class DepartmentController {
+    @GetMapping("/plan-paging-department")
+    public ResponseEntity<ListResponse<DepartmentPaginate>> getListDepartmentPaging(
+            @RequestParam(required = false) String page,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortType
+    ){
+        ListResponse<DepartmentPaginate> listResponse = new ListResponse<>();
+        listResponse.setData(List.of(
+                DepartmentPaginate.builder()
+                        .departmentId(1L)
+                        .name("Department 1")
+                        .build(),
+                DepartmentPaginate.builder()
+                        .departmentId(2L)
+                        .name("Department 2")
+                        .build(),
+                DepartmentPaginate.builder()
+                        .departmentId(3L)
+                        .name("Department 3")
+                        .build(),
+                DepartmentPaginate.builder()
+                        .departmentId(3L)
+                        .name("Department 3")
+                        .build(),
+                DepartmentPaginate.builder()
+                        .departmentId(4L)
+                        .name("Department 4")
+                        .build()
+        ));
+
+        listResponse.setPagination(Pagination.builder()
+                .count(100)
+                .page(10)
+                .displayRecord(0)
+                .numPages(1)
+                .build());
+
+        return ResponseEntity.ok(listResponse);
+    }
+
+}
