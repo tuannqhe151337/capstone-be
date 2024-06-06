7c025c946d38facf0e4574a6fd0eea44b1c1b4f2
hoannbhe160107 <hoannbhe160107@fpt.edu.vn>
2024-06-06 17:41:59 +0700

    [mock] add plan paging term mock API

---
+++ src/main/java/com/example/capstone_project/controller/TermController.java
@@ -0,0 +1,78 @@
+package com.example.capstone_project.controller;
+
+import com.example.capstone_project.controller.responses.ListResponse;
+import com.example.capstone_project.controller.responses.Pagination;
+import com.example.capstone_project.controller.responses.term.paginate.StatusResponse;
+import com.example.capstone_project.controller.responses.term.paginate.TermPaginateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/term")
+public class TermController {
+    @GetMapping("/plan-paging-term")
+    public ResponseEntity<ListResponse<TermPaginateResponse>> getListTermPaging(
+            @RequestParam(required = false) String query,
+            @RequestParam(required = false) String page,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortType
+    ) {
+        ListResponse<TermPaginateResponse> listResponse = new ListResponse<>();
+        listResponse.setData(List.of(
+                TermPaginateResponse.builder()
+                        .termId(1L)
+                        .name("Term name 1")
+                        .status(StatusResponse.builder()
+                                .statusId(1L)
+                                .name("New").build()
+                        )
+                        .startDate(LocalDateTime.now())
+                        .endDate(LocalDateTime.of(2024, 10, 2, 5, 4, 0)).build(),
+                TermPaginateResponse.builder()
+                        .termId(2L)
+                        .name("Term name 2")
+                        .status(StatusResponse.builder()
+                                .statusId(2L)
+                                .name("Approved").build()
+                        )
+                        .startDate(LocalDateTime.now())
+                        .endDate(LocalDateTime.of(2024, 10, 2, 5, 4, 0)).build(),
+                TermPaginateResponse.builder()
+                        .termId(3L)
+                        .name("Term name 3")
+                        .status(StatusResponse.builder()
+                                .statusId(3L)
+                                .name("Waiting for review").build()
+                        )
+                        .startDate(LocalDateTime.now())
+                        .endDate(LocalDateTime.of(2024, 10, 2, 5, 4, 0)).build(),
+                TermPaginateResponse.builder()
+                        .termId(4L)
+                        .name("Term name 4")
+                        .status(StatusResponse.builder()
+                                .statusId(1L)
+                                .name("Reviewed").build()
+                        )
+                        .startDate(LocalDateTime.now())
+                        .endDate(LocalDateTime.of(2024, 10, 2, 5, 4, 0)).build()
+        ));
+
+        listResponse.setPagination(Pagination.builder()
+                .count(100)
+                .page(10)
+                .displayRecord(0)
+                .numPages(1)
+                .build());
+
+        return ResponseEntity.ok(listResponse);
+    }
+}
