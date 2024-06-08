747bef5c86dce12375d36426afed4a49fa7da242
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-06-08 19:36:50 +0700

    [mock] GET PLANS per TERM with pagination response, change obscure property names

--- src/main/java/com/example/capstone_project/utils/mapper/term/update/UpdateTermBodyToTermDetailResponseMapper.java
+++ src/main/java/com/example/capstone_project/utils/mapper/term/update/UpdateTermBodyToTermDetailResponseMapper.java
@@ -2,7 +2,7 @@ package com.example.capstone_project.utils.mapper.term.update;
 
 import com.example.capstone_project.controller.body.term.delete.DeleteTermBody;
 import com.example.capstone_project.controller.body.term.update.UpdateTermBody;
-import com.example.capstone_project.controller.responses.term.get.TermDetailResponse;
+import com.example.capstone_project.controller.responses.term.get_plans.TermDetailResponse;
 import com.example.capstone_project.controller.responses.user.create.UserDetailResponse;
 import com.example.capstone_project.entity.Term;
 import com.example.capstone_project.entity.User;
