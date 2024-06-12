46f9a551cd92857d0282b008814f4cf41a87c18f
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-06-12 21:44:29 +0700

    [mock] term status list down menu in term management

--- src/main/java/com/example/capstone_project/controller/responses/term/getTermStatus/TermStatusResponse.java
+++ src/main/java/com/example/capstone_project/controller/responses/term/getTermStatus/TermStatusResponse.java
@@ -1,16 +1,23 @@
 package com.example.capstone_project.controller.responses.term.getTermStatus;
 
 
+import com.example.capstone_project.entity.Term;
+import com.example.capstone_project.utils.enums.TermCode;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
-@AllArgsConstructor
-@RequiredArgsConstructor
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TermStatusResponse {
-
+    private Long id;
+    private String name;
+    private TermCode code;
 
 }
