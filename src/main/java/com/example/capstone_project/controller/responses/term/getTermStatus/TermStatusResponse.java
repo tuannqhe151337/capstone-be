2aa1974d876fe2bcc1472c30ba379f69f2c3a183
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-06-12 20:29:21 +0700

    term status filter : menu down filter, filter term by term status

---
+++ src/main/java/com/example/capstone_project/controller/responses/term/getTermStatus/TermStatusResponse.java
@@ -0,0 +1,16 @@
+package com.example.capstone_project.controller.responses.term.getTermStatus;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class TermStatusResponse {
+
+
+}
