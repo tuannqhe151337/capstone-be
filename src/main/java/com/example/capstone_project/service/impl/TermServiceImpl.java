41fb8bc0a2864ea730738972b870b08c1cde614f
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-06-13 14:34:55 +0700

    [bug] fix authorization

--- src/main/java/com/example/capstone_project/service/impl/TermServiceImpl.java
+++ src/main/java/com/example/capstone_project/service/impl/TermServiceImpl.java
@@ -41,7 +41,8 @@ public class TermServiceImpl implements TermService {
     public List<Term> getListTermPaging(String query, Pageable pageable) {
         long userId = UserHelper.getUserId();
 
-        if (userAuthorityRepository.get(userId).contains(AuthorityCode.VIEW_PLAN.getValue())) {
+        if (userAuthorityRepository.get(userId).contains(AuthorityCode.VIEW_PLAN.getValue())
+            || userAuthorityRepository.get(userId).contains(AuthorityCode.VIEW_TERM.getValue())) {
             return termRepository.getListTermPaging(query, pageable);
 
         }
