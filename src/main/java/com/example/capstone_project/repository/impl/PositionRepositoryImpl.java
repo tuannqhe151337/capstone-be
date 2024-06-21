6be4d1941ebd666c003ddf20379f78271f4d9e90
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-06-21 21:11:09 +0700

    [feature] resolve cm

--- src/main/java/com/example/capstone_project/repository/impl/PositionRepositoryImpl.java
+++ src/main/java/com/example/capstone_project/repository/impl/PositionRepositoryImpl.java
@@ -45,16 +45,11 @@ public class PositionRepositoryImpl implements CustomPositionRepository {
                 hql += " ";
             }
         }
-        EntityGraph<Position> entityGraph = entityManager.createEntityGraph(Position.class);
-
-        // Thêm các thuộc tính cần nạp vào EntityGraph
-        entityGraph.addAttributeNodes("id", "name", "isDelete");
         // Run query
         return entityManager.createQuery(hql, Position.class)
                 .setParameter("query", "%" + query + "%")
                 .setFirstResult((pageable.getPageNumber() - 1) * pageable.getPageSize()) // We can't use pagable.getOffset() since they calculate offset by taking pageNumber * pageSize, we need (pageNumber - 1) * pageSize
                 .setMaxResults(pageable.getPageSize())
-                .setHint("jakarta.persistence.fetchgraph", entityGraph)
                 .getResultList();
     }
 }
