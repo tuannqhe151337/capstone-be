2518b39838321647d8492c6cd248a5ae9e801fa5
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-07-03 10:00:51 +0700

    [feature] @EnableAsync

--- src/main/java/com/example/capstone_project/CapstoneProjectApplication.java
+++ src/main/java/com/example/capstone_project/CapstoneProjectApplication.java
@@ -2,10 +2,12 @@ package com.example.capstone_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class CapstoneProjectApplication {
 
     public static void main(String[] args) {
