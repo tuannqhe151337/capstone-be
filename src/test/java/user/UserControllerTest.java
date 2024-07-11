83da6a4d01ce96fc20ad3a0808a21fd7e647ea75
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-07-11 21:01:29 +0700

    [test] controller list user

--- src/test/java/user/UserControllerTest.java
+++ src/test/java/user/UserControllerTest.java
@@ -1,6 +1,9 @@
 package user;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -8,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.example.capstone_project.config.JacksonConfig;
 import com.example.capstone_project.controller.UserController;
 import com.example.capstone_project.controller.body.user.create.CreateUserBody;
+import com.example.capstone_project.controller.responses.plan.UserResponse;
 import com.example.capstone_project.controller.responses.user.detail.UserDetailResponse;
 import com.example.capstone_project.entity.*;
 import com.example.capstone_project.service.impl.UserServiceImpl;
@@ -17,6 +21,7 @@ import com.example.capstone_project.utils.exception.UnauthorizedException;
 import com.example.capstone_project.utils.mapper.user.create.CreateUserBodyMapperImpl;
 import com.example.capstone_project.utils.mapper.user.detail.DetailUserResponseMapperImpl;
 
+import com.example.capstone_project.utils.mapper.user.list.ListUserResponseMapperImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -31,6 +36,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -61,6 +67,7 @@ public class UserControllerTest {
     private Long actorId;
 
     private User user;
+    private User user2;
 
     private Position position;
     private Department department;
@@ -75,6 +82,7 @@ public class UserControllerTest {
 
 
         userId = 1L;
+
         actorId = 2L;
 
         // Mock the SecurityContextHolder to return a valid user ID
@@ -116,6 +124,21 @@ public class UserControllerTest {
                 .address("Ha Noi")
                 .isDelete(false)
                 .phoneNumber("0999988877")
+                .build();
+
+        user2 = User.builder()
+                .id(2L)
+                .username("username2")
+                .fullName("NutNUnu")
+                .password("password")
+                .role(role)
+                .department(department)
+                .position(position)
+                .dob(LocalDateTime.of(2002, 11, 11, 0, 0, 0, 0))
+                .email("mailho0021@gmail.com")
+                .address("Ha Noi")
+                .isDelete(false)
+                .phoneNumber("0999965877")
                 .build();
     }
 
@@ -221,6 +244,55 @@ public class UserControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.field").value("email"))
                 .andExpect(jsonPath("$.message").value("emails already exists"));
+    }
+
+    //USER LIST TEST
+    @Test
+    public void testGetAllUsers() throws Exception {
+
+        List<User> userList = Arrays.asList(user, user2);
+
+        when(userServiceImpl.getAllUsers(anyString(), any(Pageable.class)))
+                .thenReturn(userList);
+
+        when(userServiceImpl.countDistinct(anyString()))
+                .thenReturn((long) userList.size());
+
+        // Perform the GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/api/user")
+                        .param("query", "")
+                        .param("page", "1")
+                        .param("size", "1")
+                        .param("sortBy", "username")
+                        .param("sortType", "asc")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data.length()").value(userList.size()))
+                .andExpect(jsonPath("$.pagination.totalRecords").value(userList.size()))
+                .andExpect(jsonPath("$.pagination.numPages").value(2))
+                .andExpect(jsonPath("$.data[0].userId").value(user.getId()))
+                .andExpect(jsonPath("$.data[0].username").value(user.getUsername()))
+                .andExpect(jsonPath("$.data[0].email").value(user.getEmail()))
+                .andExpect(jsonPath("$.data[0].role.id").value(user.getRole().getId()))
+                .andExpect(jsonPath("$.data[0].role.code").value(user.getRole().getCode()))
+                .andExpect(jsonPath("$.data[0].role.name").value(user.getRole().getName()))
+                .andExpect(jsonPath("$.data[0].department.id").value(user.getDepartment().getId()))
+                .andExpect(jsonPath("$.data[0].department.name").value(user.getDepartment().getName()))
+                .andExpect(jsonPath("$.data[0].position.id").value(user.getPosition().getId()))
+                .andExpect(jsonPath("$.data[0].position.name").value(user.getPosition().getName()))
+                .andExpect(jsonPath("$.data[0].deactivate").value(user.getIsDelete()))
+                .andExpect(jsonPath("$.data[1].userId").value(user2.getId()))
+                .andExpect(jsonPath("$.data[1].username").value(user2.getUsername()))
+                .andExpect(jsonPath("$.data[1].email").value(user2.getEmail()))
+                .andExpect(jsonPath("$.data[1].role.id").value(user2.getRole().getId()))
+                .andExpect(jsonPath("$.data[1].role.code").value(user2.getRole().getCode()))
+                .andExpect(jsonPath("$.data[1].role.name").value(user2.getRole().getName()))
+                .andExpect(jsonPath("$.data[1].department.id").value(user2.getDepartment().getId()))
+                .andExpect(jsonPath("$.data[1].department.name").value(user2.getDepartment().getName()))
+                .andExpect(jsonPath("$.data[1].position.id").value(user2.getPosition().getId()))
+                .andExpect(jsonPath("$.data[1].position.name").value(user2.getPosition().getName()))
+                .andExpect(jsonPath("$.data[1].deactivate").value(user2.getIsDelete()));
     }
 
 }
