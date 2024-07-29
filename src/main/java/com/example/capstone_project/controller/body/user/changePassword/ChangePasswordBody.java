c90b5d69d132cab7e16a748a652a322ed5fa80be
giangdvhe163178 <giangdvhe163178@fpt.edu.vn>
2024-07-29 16:02:23 +0700

    [update]

--- src/main/java/com/example/capstone_project/controller/body/user/changePassword/ChangePasswordBody.java
+++ src/main/java/com/example/capstone_project/controller/body/user/changePassword/ChangePasswordBody.java
@@ -2,6 +2,7 @@ package com.example.capstone_project.controller.body.user.changePassword;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Data
@@ -14,6 +15,12 @@ public class ChangePasswordBody {
     @NotEmpty(message = "Password cannot be null")
     private String oldPassword;
     @NotEmpty(message = "New password cannot be null")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,}$",
+            message = "New password must be at least 8 characters long," +
+                    " contain at least one special character," +
+                    " one uppercase letter, and one lowercase letter"
+    )
     private String newPassword;
 
 }
