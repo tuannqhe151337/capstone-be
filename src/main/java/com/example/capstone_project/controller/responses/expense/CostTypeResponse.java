d09e66d29e699e106c9d6f5a291178063eef045a
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-07-31 11:32:36 +0700

    [feature] add createAt and updateAt in cost type response

--- src/main/java/com/example/capstone_project/controller/responses/expense/CostTypeResponse.java
+++ src/main/java/com/example/capstone_project/controller/responses/expense/CostTypeResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Data
 @AllArgsConstructor
@@ -12,4 +14,6 @@ import lombok.NoArgsConstructor;
 public class CostTypeResponse {
     private long costTypeId;
     private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
