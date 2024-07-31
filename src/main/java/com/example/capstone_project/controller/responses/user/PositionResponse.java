b72a2caf1fb597dfcf8ff840a3b3eec79022642b
tunmhe150840 <tunmhe150840@fpt.edu.vn>
2024-07-31 13:09:39 +0700

    [feature] add createAt and updateAt in position response

--- src/main/java/com/example/capstone_project/controller/responses/user/PositionResponse.java
+++ src/main/java/com/example/capstone_project/controller/responses/user/PositionResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,4 +14,6 @@ import lombok.NoArgsConstructor;
 public class PositionResponse {
     private Long id;
     private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
